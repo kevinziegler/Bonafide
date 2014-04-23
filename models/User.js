78253d9fdfa36a834769a5d8cc2486d6a77f8754
Kevin Ziegler <kevin.ziegler@briggo.com>
2014-04-23 00:51:15 -0500

    Add Mongoose model for User objects.

---
+++ models/User.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    passportLocalMongoose = require('passport-local-mongoose');
+
+
+var userSchema = new Schema({
+    email : { type: String, unique: true, required: true},
+    name: { 
+        first: String,
+        last: String,
+    },
+    registered : { type: Date, default: Date.now }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+
+/* Validation Methods */
+/*
+userSchema.path('email').validate(function (email) {
+    // FIXME: Find a better regex to use here.  
+    var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRegex.test(email.text);
+}, 'A valid email is required for registration'); 
+*/
+
+/* Virtuals */
+userSchema.virtual('name.full').get(function () {
+    return this.name.first + ' ' + this.name.last;
+});
+
+userSchema.virtual('name.reverse').get(function() {
+    return this.name.last + ', ' + this.name.first;
+});
+
+exports.User = mongoose.model('User', userSchema);
