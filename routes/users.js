a3aaa1217e337767313b606917a9cf6dd28b6329
Kevin Ziegler <kevin.ziegler@briggo.com>
2014-04-23 01:22:43 -0500

    Add passport code to register new users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var UserModel = require('../models/User.js'),
+    User = UserModel.User;
+
+//var UserModel = require('models/User.js'),
+    //User = UserModel.User;
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  User.find({}, function(err, users) {
+        res.render('users', { 
+            title : 'Users',  
+            users : users 
+        });
+  });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', {});
+});
+
+router.post('/register', function(req, res) {
+    console.log('Received registration request: ' + req.body.email);
+    user = new User({
+        username : req.body.email, 
+        email : req.body.email,
+        name : {
+            first : req.body.first_name,
+            last : req.body.last_name,
+        }
+    });
+
+    User.register(user, req.body.password, function (err, user) {
+        if (err) {
+            return res.render('register', {user : user});
+        }
+        
+        passport.authenticate('local')(req, res, function() {
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/:id', function(req,res) {
+
 });
 
 module.exports = router;
